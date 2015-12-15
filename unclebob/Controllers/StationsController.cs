47657d6fdbca479cb2b2e43b9a8bec122167c862
Erik Morén <erik.moren@gmail.com>
2015-12-15 14:17:27 +0100

    .

---
+++ unclebob/Controllers/StationsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using unclebob.Models;
+
+namespace unclebob.Controllers
+{
+    public class StationsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Station
+        public IQueryable<Station> GetStation()
+        {
+            return db.Station;
+        }
+
+        // GET: api/Station/5
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult GetStation(int id)
+        {
+            Station station = db.Station.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(station);
+        }
+
+        // PUT: api/Station/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutStation(int id, Station station)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != station.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(station).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Station
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult PostStation(Station station)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Station.Add(station);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = station.Id }, station);
+        }
+
+        // DELETE: api/Station/5
+        [ResponseType(typeof(Station))]
+        public IHttpActionResult DeleteStation(int id)
+        {
+            Station station = db.Station.Find(id);
+            if (station == null)
+            {
+                return NotFound();
+            }
+
+            db.Station.Remove(station);
+            db.SaveChanges();
+
+            return Ok(station);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool StationExists(int id)
+        {
+            return db.Station.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ unclebob/Models/Station.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace unclebob.Models
+{
+    public class Station
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+
+    }
+}
